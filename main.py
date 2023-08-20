e58e258915cbb1434bb18afa77177a2a6c5ee40a
Acon235 <acon235@gmail.com>
2023-08-20 17:15:45 -0500

    current experimental code

--- main.py
+++ main.py
@@ -41,15 +41,33 @@ class PlantController:
             self.pump_cycle_time = config.getint('PumpSettings', 'pumpCycleTime')  # number of hours between pump cycles
             self.pump_on_interval = config.getint('PumpSettings', 'pumpOnInterval')  # number of minutes pump stays on
 
-            self.measure_interval = config.getint('MeasurementSettings', 'measureInterval')  # minutes between measurements
+            self.adjustment_interval = config.getint('AdjustmentSettings', 'adjustmentInterval')  # minutes between adjustments
 
             self.light_pin = config.getint('GPIOSettings', 'lightPin')
             self.water_pin = config.getint('GPIOSettings', 'waterPin')
             self.stop_button_pin = config.getint('GPIOSettings', 'stopButtonPin')
 
+            # Add the GPIO pin numbers to your config file and load them here
+
+            self.ph_sensor_pin_in = config.getint('GPIOSettings', 'phSensorPinIn')
+            self.ph_sensor_pin_out = config.getint('GPIOSettings', 'phSensorPinOut')
+            self.ec_sensor_pin = config.getint('GPIOSettings', 'ecSensorPin')
+            self.temp_sensor_pin = config.getint('GPIOSettings', 'tempSensorPin')
+            self.water_level_sensor_pin = config.getint('GPIOSettings', 'waterLevelSensorPin')
+            self.humidity_sensor_pin = config.getint('GPIOSettings', 'humiditySensorPin')
+
+            self.humidifier_pin = config.getint('GPIOSettings', 'humidifierPin')  # add GPIO pin for humidifier
+            self.temperature_controller_pin = config.getint('GPIOSettings',
+                                                            'temperatureControllerPin')  # add GPIO pin for temperature controller
+
+            # Setup GPIO for sensors and controllers
             gpio.setup(self.light_pin, gpio.OUT, initial=gpio.LOW)
             gpio.setup(self.water_pin, gpio.OUT, initial=gpio.LOW)
             gpio.setup(self.stop_button_pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+            # gpio.setup(self.ph_sensor_pin_in, gpio.IN)
+            # gpio.setup(self.ph_sensor_pin_out, gpio.OUT, initial=gpio.LOW)
+            # Add the rest of the sensor and controller GPIO setup here as input or output.
 
             self.light_on_time = None
             self.light_off_time = None
@@ -153,9 +171,57 @@ class PlantController:
 
         self.log_message("Program started")
 
-    def log_measurements(self):
-        # do temp, ph, ec measurements and log
-        self.log_message("Measurements made")
+    def read_sensors(self):
+        # Placeholder sensor readings, replace with actual sensor readings
+        ph = self.ph_sensor.read()
+        ec = self.ec_sensor.read()
+        temp = self.temp_sensor.read()
+        water_level = self.water_level_sensor.read()
+        humidity = self.humidity_sensor.read()
+
+        # Log the raw sensor readings
+        self.log_message(
+            f"PH: {ph} EC: {ec} Temp: {temp} Water level: {water_level} Humidity: {humidity}")
+
+        return {'ph': ph, 'ec': ec, 'temp': temp, 'water_level': water_level, 'humidity': humidity}
+
+    def adjust_nutrients(self, ph, ec):
+        # Placeholder for adjusting nutrients, replace with actual nutrient adjustment
+        adjusted_ph = ph  # Dummy value
+        adjusted_ec = ec  # Dummy value
+
+        self.log_message(f"Adjusted PH from {ph} to {adjusted_ph}")
+        self.log_message(f"Adjusted EC from {ec} to {adjusted_ec}")
+
+    def adjust_humidity(self, humidity):
+        # Placeholder for adjusting humidity, replace with actual humidity adjustment
+        adjusted_humidity = humidity  # Dummy value
+
+        self.log_message(f"Adjusted humidity from {humidity} to {adjusted_humidity}")
+
+    def adjust_temperature(self, temperature):
+        # Placeholder for adjusting temperature, replace with actual temperature adjustment
+        adjusted_temperature = temperature  # Dummy value
+
+        self.log_message(f"Adjusted temperature from {temperature} to {adjusted_temperature}")
+
+    def adjust_water_level(self, water_level):
+        # Placeholder for adjusting water level, replace with actual water level adjustment
+        adjusted_water_level = water_level  # Dummy value
+
+        self.log_message(f"adjusted water level from {water_level} to {adjusted_water_level}")
+
+    def make_adjustments(self):
+        # Read the sensors.
+        sensor_values = self.read_sensors()
+
+        # Adjust systems based on sensor values.
+        self.adjust_nutrients(sensor_values['ph'], sensor_values['ec'])
+        self.adjust_humidity(sensor_values['humidity'])
+        self.adjust_temperature(sensor_values['temp'])
+        self.adjust_water_level(sensor_values['water_level'])
+
+        self.log_message("All necessary adjustments made")
 
 
 if __name__ == "__main__":
@@ -173,7 +239,7 @@ if __name__ == "__main__":
         # Set up the scheduling
         schedule.every().day.at("23:45").do(plant.update_light_schedule).tag("light_update_task")
         schedule.every(plant.pump_cycle_time).hours.do(plant.pump_on).tag("pump_on_task")
-        schedule.every(plant.measure_interval).minutes.do(plant.log_measurements).tag("measurement_task")
+        schedule.every(plant.adjustment_interval).minutes.do(plant.make_adjustments).tag("adjustment_task")
 
         while True:
             schedule.run_pending()
